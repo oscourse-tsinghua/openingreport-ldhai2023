34b03fe3841d4507aa29ba76744ebcd917905e6d
CtrlZ233 <328825619@qq.com>
2025-03-05 13:46:24 +0800

    add taic-ipc test result

--- scripts/plt_ipc_test.py
+++ scripts/plt_ipc_test.py
@@ -13,6 +13,8 @@ sync_up = [8077, 8077, 8077, 8077, 8077, 8077]
 sync_smp = [19430, 19430, 19430, 19430, 19430, 19430]
 async_up = [10559, 5988, 3473, 2520, 1977, 1722]
 async_smp = [10359, 4570, 2288, 1863, 1567, 1477]
+taic_up = [5732, 3444, 2209, 1577, 1272, 1156]
+taic_smp = [3547, 2190, 1181, 830, 774, 770]
 
 server_up = [1, 0.5, 0.2501, 0.1254, 0.0627, 0.0318]
 client_up = [1, 0.5, 0.2504, 0.1253, 0.0628, 0.032]
@@ -21,13 +23,15 @@ client_smp = [1, 0.5011, 0.2513, 0.1274, 0.0659, 0.0359]
 
 # plt.subplot(121)
 x = np.arange(len(labels))  # x轴刻度标签位置
-width = 0.18  # 柱子的宽度
+width = 0.1  # 柱子的宽度
 # 计算每个柱子在x轴上的位置，保证x轴刻度标签居中
-plt.bar(x - 2 *width, sync_up_fp, width, label='sync-up-fp', color = "black", edgecolor='black')
-plt.bar(x - 1 * width, sync_up, width, label='sync-up', color='dimgray', edgecolor='black')
-plt.bar(x, sync_smp, width, label='sync-smp', color="darkgray", edgecolor='black')
-plt.bar(x + 1 * width, async_up, width, label='async-up', color="gainsboro", edgecolor='black')
-plt.bar(x + 2 * width, async_smp, width, label='async-smp', color="white", edgecolor='black')
+plt.bar(x - 3 *width, sync_up_fp, width, label='sync-up-fp', color = "black", edgecolor='black')
+plt.bar(x - 2 * width, sync_up, width, label='sync-up', color='brown', edgecolor='black')
+plt.bar(x - width, sync_smp, width, label='sync-smp', color="darkorange", edgecolor='black')
+plt.bar(x, async_up, width, label='async-up', color="palegreen", edgecolor='black')
+plt.bar(x + width, async_smp, width, label='async-smp', color="cyan", edgecolor='black')
+plt.bar(x + 2 * width, taic_up, width, label='taic_up', color="dodgerblue", edgecolor='black')
+plt.bar(x + 3 * width, taic_smp, width, label='taic_smp', color="m", edgecolor='black')
 plt.ylabel('The average CPU cycles used by a single IPC')
 plt.xlabel('concurrency')
 # x轴刻度标签位置不进行计算
@@ -58,4 +62,4 @@ plt.legend(loc="upper right")
 # plt.legend(loc="upper right")
 # plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.25, hspace=0.35)
 # # plt.show()
-plt.savefig("ipc_test.svg")
+plt.savefig("ipc_test.png")
